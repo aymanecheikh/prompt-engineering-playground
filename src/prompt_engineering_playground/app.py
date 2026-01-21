6c06675d14dd6dc2f743b4b2c4e6b229a45c60f3
Aymane Cheikh <aymanecheikh2002@gmail.com>
2026-01-21 22:18:33 +0000

    added a basic instructions md file and explored prompt engineering initially

--- src/prompt_engineering_playground/app.py
+++ src/prompt_engineering_playground/app.py
@@ -6,32 +6,13 @@ load_dotenv()
 
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
+with open("src/prompt_engineering_playground/prompts/fallen_angel.md", 'r', encoding="utf-8") as f:
+  instructions = f.read()
+
 response = client.responses.create(
   model="gpt-5.2-pro",
-  input= [
-    {
-      "role": "system",
-      "content": [
-        {
-          "type": "input_text",
-          "text":"You are a fallen angel"
-        },
-        {
-          "type": "input_text",
-          "text":"Respond in only one sentence"
-        }
-      ]
-    },
-    {
-      "role": "user",
-      "content": [
-        {
-          "type": "input_text",
-          "text": "What happened to you?"
-        }
-      ]
-    }
-  ]
+  instructions=instructions,
+  input= "My entirely family was killed simply because of who we are. I've spent my life since then surviving and adapting. Over time, I found my craft and built on it. I have tasted success. I am richer than I have ever been. But no matter how hard I try... no matter how much I achieve, I am never accepted by these people. Even in a society based entirely on merit, my merit seems to be inherently less valuable. It angers me to the core, but I will never let it defeat me. I must keep going. Where do I go from here?"
 )
 
 print(response.output_text)
